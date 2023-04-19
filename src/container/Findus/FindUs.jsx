d623da9e16b2dff487c1df73f7d2dae7ad82bed7
Dimitar Mitev <d.mitev920123@gmail.com>
2023-04-19 15:58:00 +0300

    Find Us correct

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { SubHeading } from '../../components/index.js';
+import { SubHeading } from '../../components';
 import { images } from '../../constants';
 
 const FindUs = () => (
@@ -15,7 +15,7 @@ const FindUs = () => (
             </h1>
             <div className='app__wrapper-content'>
                 <p className='p__opensans'>
-                    Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG
+                    Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 75G
                 </p>
                 <p
                     className='p__cormorant'
@@ -26,12 +26,17 @@ const FindUs = () => (
                 <p className='p__opensans'>Mon - Fri: 10:00 am - 02:00 am</p>
                 <p className='p__opensans'>Sat - Sun: 10:00 am - 03:00 am</p>
             </div>
-            <button className='custom__button' style={{ marginTop: '2rem' }}>
+            <button
+                type='button'
+                className='custom__button'
+                style={{ marginTop: '2rem' }}
+            >
                 Visit Us
             </button>
         </div>
+
         <div className='app__wrapper_img'>
-            <img src={images.findus} alt='findus' />
+            <img src={images.findus} alt='finus_img' />
         </div>
     </div>
 );
