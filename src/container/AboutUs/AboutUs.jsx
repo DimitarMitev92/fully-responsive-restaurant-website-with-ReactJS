3e06b9ad0c946fad102cdb875db43281087d7774
Dimitar Mitev <d.mitev920123@gmail.com>
2023-04-19 13:04:54 +0300

    Chef

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -9,8 +9,9 @@ const AboutUs = () => (
         id='about'
     >
         <div className='app__aboutus-overlay flex__center'>
-            <img src={images.G} alt='g letter' />
+            <img src={images.G} alt='G_overlay' />
         </div>
+
         <div className='app__aboutus-content flex__center'>
             <div className='app__aboutus-content_about'>
                 <h1 className='headtext__cormorant'>About Us</h1>
@@ -20,10 +21,9 @@ const AboutUs = () => (
                     className='spoon__img'
                 />
                 <p className='p__opensans'>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Reiciendis consequatur ad debitis adipisci cupiditate
-                    voluptate eaque vel, est ipsam sunt, maiores ipsa eos
-                    blanditiis alias laborum soluta officia consequuntur saepe!
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Quis pharetra adipiscing ultrices vulputate posuere
+                    tristique. In sed odio nec aliquet eu proin mauris et.
                 </p>
                 <button type='button' className='custom__button'>
                     Know More
@@ -42,10 +42,9 @@ const AboutUs = () => (
                     className='spoon__img'
                 />
                 <p className='p__opensans'>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Reiciendis consequatur ad debitis adipisci cupiditate
-                    voluptate eaque vel, est ipsam sunt, maiores ipsa eos
-                    blanditiis alias laborum soluta officia consequuntur saepe!
+                    Adipiscing tempus ullamcorper lobortis odio tellus arcu
+                    volutpat. Risus placerat morbi volutpat habitasse interdum
+                    mi aliquam In sed odio nec aliquet.
                 </p>
                 <button type='button' className='custom__button'>
                     Know More
